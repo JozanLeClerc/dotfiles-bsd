912df73aab0375a1409f911770bcd23a289d6094
JozanLeClerc <bousset.rudy@gmail.com>
2020-08-15 15:50:43 +0200

    Added evil-vimish-fold to Emacs

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,3 +1,3 @@
 source $HOME/Packages/git-prompt.zsh/git-prompt.zsh
 # source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh >/dev/null
-source /usr/home/jozan/Packages/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh >/dev/null
+source $HOME/Packages/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh >/dev/null
