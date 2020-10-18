809c95a3cf144c1d87b8b365c83d07821204b4d8
JozanLeClerc <bousset.rudy@gmail.com>
2020-10-18 23:45:05 +0200

    Added unison cron job

---
+++ .local/bin/ref-unison.pl
@@ -0,0 +1,39 @@
+#!/usr/local/bin/perl
+
+use strict;
+use warnings;
+
+use constant {
+	UNISON_PATH			=> '/usr/local/bin/unison',
+	NOTIFY_SEND_PATH	=> '/usr/local/bin/notify-send'
+};
+
+sub main
+{
+	system(
+		NOTIFY_SEND_PATH,
+		'unison',
+		'-u',
+		'low',
+		'Synchronizing files...'
+	);
+	system(
+		UNISON_PATH,
+		'-ui',
+		'text',
+		'/usr/home/jozan/.local/share/newsboat/cache.db',
+		'ssh://root@jozanleclerc.xyz//root/unison/.local/share/newsboat/cache.db'
+	);
+	system(
+		UNISON_PATH,
+		'-ui',
+		'text',
+		'/usr/home/jozan/.abook/addressbook',
+		'ssh://root@jozanleclerc.xyz//root/unison/.abook/addressbook'
+	);
+	exit 0;
+}
+
+main();
+
+__END__
