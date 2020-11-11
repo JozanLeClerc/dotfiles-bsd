c448b52d9f89225ed5ee096d8d152235c11b98cb
JozanLeClerc <bousset.rudy@gmail.com>
2020-11-11 20:39:05 +0100

    Update

--- .local/bin/setscreens.sh
+++ .local/bin/setscreens.sh
@@ -5,7 +5,7 @@ if xrandr | grep 'HDMI-1 connected'; then
 		xrandr --output LVDS-1 --off
 		xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 		xrandr --output DP-1 --off
-		xrandr --output VGA-1 --mode 1440x900 --pos 1920x90 --rotate normal
+		xrandr --output VGA-1 --mode 1440x900 --right-of HDMI-1 --rotate normal
 		xrandr --output HDMI-2 --off
 		xrandr --output HDMI-3 --off
 		xrandr --output DP-2 --off
