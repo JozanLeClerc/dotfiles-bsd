bb0427893abcb5ae7e57767b35efc86e4a15cbcc
JozanLeClerc <bousset.rudy@gmail.com>
2020-11-12 21:22:34 +0100

    Update

--- .local/bin/dunst_espeak.sh
+++ .local/bin/dunst_espeak.sh
@@ -3,5 +3,5 @@
 [ "$5" = "LOW" ] && exit
 echo $3 > $HOME/file
 [ "$3" ] || exit
-words=$(printf "$3" | sed 's/<b>//g' | sed 's/<\/b>//g' | sed 's/[^a-zA-Z0-9?!., ]//g')
+words=$(printf "$3" | sed 's/<b>//g' | sed 's/<\/b>//g' | sed 's/[^a-zA-Z0-9?!.,% ]//g')
 espeak "$words"
