f3d2e39a4cd3c7c138207f9a8453950a33d92363
JozanLeClerc <bousset.rudy@gmail.com>
2020-11-14 16:59:44 +0100

    Less annoying dunst_espeak

--- .local/bin/dunst_espeak.sh
+++ .local/bin/dunst_espeak.sh
@@ -2,6 +2,9 @@
 
 [ "$5" = "LOW" ] && exit
 [ "$3" ] || exit
-words=$(printf "$3" | sed 's/<b>//g' | sed 's/<\/b>//g' | sed 's/[^a-zA-Z0-9?!.,% ]//g')
-words=$(printf "%s\n" "$words" | awk '{for (i=1;i<11;i++)print $i }' | tr '\n' ' ')
+words=$(printf "%s" "$3" |
+	sed 's/<b>//g' |
+	sed 's/<\/b>//g' |
+	sed 's/[^a-zA-Z0-9?!.,% ]//g')
+words=$(printf "%s" "$words" | gsed 's/^[0-9]\+//g')
 espeak "$words"
