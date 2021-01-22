1c49cd9eafc1e34675bf001dbeb92b4c32094d7a
JozanLeClerc <bousset.rudy@gmail.com>
2021-01-22 23:25:18 +0100

    Scripts update

--- .local/bin/sync-elfeed.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/dash
-
-date=$(date)
-git -C ~/.elfeed add . >/dev/null 2>&1
-git -C ~/.elfeed commit -m "$date" >/dev/null 2>&1 || exit 1
-git -C ~/.elfeed push origin master >/dev/null 2>&1
-exit
