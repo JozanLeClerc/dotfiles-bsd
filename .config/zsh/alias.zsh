21a9d76fa507f21483e977a196b940f718096ab5
JozanLeClerc <bousset.rudy@gmail.com>
2021-03-16 16:06:50 +0100

    Back to firefox

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -11,66 +11,66 @@
 # ==     =====   ====   ==
 # ========================
 
-alias exa='exa --color=always --group-directories-first'
-alias ls='lsd -l --date relative --group-dirs first'
+alias exa='exa --color=always --group-directories-first' \
+	ls='lsd -l --date relative --group-dirs first'
 tree() {
 	exa -T $@ | less
 }
-alias c='clear'
-alias less='less --tabs 4'
-alias bc='bc -l'
-alias mkf='gmake fclean'
-alias mkc='gmake clean'
-alias mk='gmake -j5'
-alias bmkc='make clean'
-alias bmk='make -j5'
-alias web='w3m https://duckduckgo.com/'
-alias mpv='mpv --audio-channels=stereo'
-alias cp='cp -iv'
-alias mv='mv -iv'
-alias rm='rm -v'
-alias ln='ln -v'
-alias df='df -h'
-alias du='du -h'
-alias mkdir='mkdir -v'
-alias rmdir='rmdir -v'
-alias chmod='chmod -v'
-alias chown='chown -v'
-alias mount='mount -v'
-alias rsync='rsync -v -h -P'
-alias grep='grep --color=always'
-alias rgrep='grep -R -n -H'
-alias vifm='TERM=xterm-256color vifm'
-alias fetch='torify fetch'
-alias fsl='fossil'
-alias diff='colordiff'
-alias ctags='uctags "--exclude=.ccls*"'
-alias yt='straw-viewer --api=auto -C -f'
-alias yta='straw-viewer --api=auto -C -n'
-alias gyt='gtk-straw-viewer'
-alias ytdl='youtube-dl --add-metadata'
-alias ytdlist='youtube-dl -F'
-alias ytdl1080="youtube-dl --add-metadata -f '[height<=1080]'"
-alias dgit='git --git-dir=$HOME/docs/dotfiles-bsd --work-tree=$HOME'
+alias c='clear' \
+	less='less --tabs 4' \
+	bc='bc -l' \
+	mkf='gmake fclean' \
+	mkc='gmake clean' \
+	mk='gmake -j5' \
+	bmkc='make clean' \
+	bmk='make -j5' \
+	web='w3m https://duckduckgo.com/' \
+	mpv='mpv --audio-channels=stereo' \
+	cp='cp -iv' \
+	mv='mv -iv' \
+	rm='rm -v' \
+	ln='ln -v' \
+	df='df -h' \
+	du='du -h' \
+	mkdir='mkdir -v' \
+	rmdir='rmdir -v' \
+	chmod='chmod -v' \
+	chown='chown -v' \
+	mount='mount -v' \
+	rsync='rsync -v -h -P' \
+	grep='grep --color=always' \
+	rgrep='grep -R -n -H' \
+	vifm='TERM=xterm-256color vifm' \
+	fetch='torify fetch' \
+	fsl='fossil' \
+	diff='colordiff' \
+	ctags='uctags "--exclude=.ccls*"' \
+	yt='straw-viewer --api=auto -C -f' \
+	yta='straw-viewer --api=auto -C -n' \
+	gyt='gtk-straw-viewer' \
+	ytdl='youtube-dl --add-metadata' \
+	ytdlist='youtube-dl -F' \
+	ytdl1080="youtube-dl --add-metadata -f '[height<=1080]'" \
+	dgit='git --git-dir=$HOME/docs/dotfiles-bsd --work-tree=$HOME'
 confgit() {
 	p=$(pwd)
 	cd /
 	git --git-dir=$HOME/docs/conffiles-bsd --work-tree=/ $@
 	cd $p
 }
-alias vim='nvim'
-alias gst='git status'
-alias ga='git add'
-alias gco='git commit -m'
-alias gpp='git push'
-alias gpo='git push origin'
-alias srcnt='find . -type f -name "*.c" -exec cat {} \; | sed "/^\//d" | sed "/^\*/d" | sed "/^ \*/d" | sed "/^\/\//d" | sed "/^$/d" | wc -l | tr -d " "'
-alias startdocker='sudo service vboxnet onerestart && docker-machine start docker-home'
-alias stopdocker='docker-machine stop docker-home'
-alias envdocker='eval $(docker-machine env docker-home)'
-alias vimz='nvim $(fzf --preview="head -$FZF_PREVIEW_LINES {}")'
-alias v='nvim $(fzf --preview="head -$FZF_PREVIEW_LINES {}")'
-alias tohex='printf "0x%x\n"'
+alias vim='nvim' \
+	gst='git status' \
+	ga='git add' \
+	gco='git commit -m' \
+	gpp='git push' \
+	gpo='git push origin' \
+	srcnt='find . -type f -name "*.c" -exec cat {} \; | sed "/^\//d" | sed "/^\*/d" | sed "/^ \*/d" | sed "/^\/\//d" | sed "/^$/d" | wc -l | tr -d " "' \
+	startdocker='sudo service vboxnet onerestart && docker-machine start docker-home' \
+	stopdocker='docker-machine stop docker-home' \
+	envdocker='eval $(docker-machine env docker-home)' \
+	vimz='nvim $(fzf --preview="head -$FZF_PREVIEW_LINES {}")' \
+	v='nvim $(fzf --preview="head -$FZF_PREVIEW_LINES {}")' \
+	tohex='printf "0x%x\n"'
 vimbin() {
 	p=$(pwd)
 	cd "$HOME"/.local/bin || return
@@ -130,28 +130,28 @@ alias bi='cd $HOME/.local/bin && ls'
 twi() {
 	mpview https://twitch.tv/"$1";
 }
-alias nb='torify newsboat'
-alias mutt='neomutt'
-alias sxiv='sxiv -b -a'
-alias pinfo='pkg info -x'
-alias psearch='pkg search'
-alias highlight='highlight -Oxterm256 -t4'
-alias hi='highlight'
+alias nb='torify newsboat' \
+	mutt='neomutt' \
+	sxiv='sxiv -b -a' \
+	pinfo='pkg info -x' \
+	psearch='pkg search' \
+	highlight='highlight -Oxterm256 -t4' \
+	hi='highlight'
 hil() {
 	highlight -Oxterm256 -t4 "$1" | less
 }
 search() {
 	make -C /usr/ports search name=$1 | grep 'Path:';
 }
-alias watch='cmdwatch'
-alias tsd='transmission-daemon'
-alias tsm='transmission-remote'
-alias rsox='sox -t oss default'
-alias calcurse='calcurse -C $XDG_CONFIG_HOME/calcurse -D $XDG_DATA_HOME/calcurse'
-alias open='xdg-open'
-alias abook='abook -C $XDG_CONFIG_HOME/abook/abookrc --datafile $XDG_DATA_HOME/abook/addressbook'
-alias dosbox='dosbox -conf $XDG_CONFIG_HOME/dosbox/dosbox.conf'
-alias svn='svn --config-dir $XDG_CONFIG_HOME/subversion'
-alias gpg2='gpg2 --homedir $XDG_DATA_HOME/gnupg'
-alias yarn='yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config'
-alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
+alias watch='cmdwatch' \
+	tsd='transmission-daemon' \
+	tsm='transmission-remote' \
+	rsox='sox -t oss default' \
+	calcurse='calcurse -C $XDG_CONFIG_HOME/calcurse -D $XDG_DATA_HOME/calcurse' \
+	open='xdg-open' \
+	abook='abook -C $XDG_CONFIG_HOME/abook/abookrc --datafile $XDG_DATA_HOME/abook/addressbook' \
+	dosbox='dosbox -conf $XDG_CONFIG_HOME/dosbox/dosbox.conf' \
+	svn='svn --config-dir $XDG_CONFIG_HOME/subversion' \
+	gpg2='gpg2 --homedir $XDG_DATA_HOME/gnupg' \
+	yarn='yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config' \
+	wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
