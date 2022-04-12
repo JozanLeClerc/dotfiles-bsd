a2ca771a797a295938a0ebae7f88f1173a8101c7
JozanLeClerc <bousset.rudy@gmail.com>
2022-04-12 15:16:39 +0200

    update

--- .local/bin/setscreens.sh
+++ .local/bin/setscreens.sh
@@ -44,16 +44,15 @@
 #
 # Script to set screens, loaded at startx only.
 
-if xrandr | grep 'HDMI-1 connected'; then
-	if xrandr | grep 'VGA-1 connected'; then
-		xrandr --output LVDS-1 --off
-		xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-		xrandr --output DP-1 --off
-		xrandr --output VGA-1 --mode 1440x900 --pos 1920x180 --rotate normal
-		xrandr --output HDMI-2 --off
-		xrandr --output HDMI-3 --off
-		xrandr --output DP-2 --off
-		xrandr --output DP-3 --off
+if xrandr | grep 'HDMI-1 connected' && xrandr | grep 'VGA-1 connected'; then
+	xrandr --output LVDS-1 --off
+	xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+	xrandr --output DP-1 --off
+	xrandr --output VGA-1 --mode 1440x900 --pos 1920x180 --rotate normal
+	xrandr --output HDMI-2 --off
+	xrandr --output HDMI-3 --off
+	xrandr --output DP-2 --off
+	xrandr --output DP-3 --off
 else
 	xrandr --output LVDS-1 --primary --mode 1366x768 --pos 0x0 --rotate normal
 	xrandr --output VGA-1 --off
