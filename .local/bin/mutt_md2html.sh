aa5bcb518d4f6b25e4ebe3f1f59fee74b9ab327f
Joe <bousset.rudy@gmail.com>
2022-07-06 12:31:06 +0200

    Update

---
+++ .local/bin/mutt_md2html.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+commandsFile="/tmp/neomutt-commands"
+markdownFile="/tmp/neomutt-markdown"
+htmlFile="/tmp/neomutt.html"
+
+cat - > "$markdownFile"
+echo -n "push " > "$commandsFile"
+
+pandoc -f markdown -t html5 --standalone "$markdownFile" > "$htmlFile"
+
+# Attach the html file
+echo -n "<attach-file>\"$htmlFile\"<enter>" >> "$commandsFile"
+
+# Set it as inline
+echo -n "<toggle-disposition>" >> "$commandsFile"
+
+# Tell neomutt to delete it after sending
+echo -n "<toggle-unlink>" >> "$commandsFile"
+
+# Select both the html and markdown files
+echo -n "<tag-entry><previous-entry><tag-entry>" >> "$commandsFile"
+
+# Group the selected messages as alternatives
+echo -n "<group-alternatives>" >> "$commandsFile"
