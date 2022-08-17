4f4501ba225f812b56ef39788b1146539d3e8925
Joe <bousset.rudy@gmail.com>
2022-08-17 14:42:49 +0200

    update

--- .local/bin/fetch_mail.sh
+++ .local/bin/fetch_mail.sh
@@ -8,7 +8,7 @@ export PASSWORD_STORE_DIR=/home/jozan/.local/share/pass
 killall mbsync >/dev/null 2>&1
 notify-send -u low -t 3000 'mbsync' '  fetching mail...' >/dev/null 2>&1
 {
-	mbsync -q -a -c /usr/home/jozan/.config/mbsync/mbsyncrc ||
+	mbsync -a -c /usr/home/jozan/.config/mbsync/mbsyncrc ||
 		notify-send -u low -t 6000 'mbsync' '  failed to fetch mail' >/dev/null 2>&1 &&
 		(sleep 1; kill -74 $(pidof dwmblocks) >/dev/null 2>&1)
 }&
