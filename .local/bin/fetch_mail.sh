9addbecbf31bab7088d3089a99964eeb3c6c7928
Joe <bousset.rudy@gmail.com>
2022-08-17 14:37:44 +0200

    update

--- .local/bin/fetch_mail.sh
+++ .local/bin/fetch_mail.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+set -m
+
 export GNUPGHOME=/home/jozan/.local/share/gnupg
 export PASSWORD_STORE_DIR=/home/jozan/.local/share/pass 
 
 killall mbsync >/dev/null 2>&1
 notify-send -u low -t 3000 'mbsync' '  fetching mail...' >/dev/null 2>&1
-mbsync -q -a -c /usr/home/jozan/.config/mbsync/mbsyncrc || notify-send -u low -t 6000 'mbsync' '  failed to fetch mail' >/dev/null 2>&1
+{
+	mbsync -q -a -c /usr/home/jozan/.config/mbsync/mbsyncrc ||
+		notify-send -u low -t 6000 'mbsync' '  failed to fetch mail' >/dev/null 2>&1 &&
+		(sleep 1; kill -74 $(pidof dwmblocks) >/dev/null 2>&1)
+}&
+gsleep 0.2
+kill -74 $(pidof dwmblocks) >/dev/null 2>&1
+fg
