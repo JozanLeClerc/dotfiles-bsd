fc59e659114f29ddb3cc63a4cc6fd543fb946c31
Joe <rrbo@proton.me>
2023-01-12 14:45:51 +0100

    up

--- .local/bin/fetch_mail.sh
+++ .local/bin/fetch_mail.sh
@@ -17,14 +17,14 @@ else
 	pre_count="$((pre_count + mb1 + mb2))"
 fi
 killall mbsync >/dev/null 2>&1
-# notify-send -u low -t 3000 'mbsync' '  fetching mail...' >/dev/null 2>&1
-{
+notify-send -u low -t 3000 'mbsync' '  fetching mail...' >/dev/null 2>&1
+# {
 	mbsync -a -c /usr/home/jozan/.config/mbsync/mbsyncrc ||
 		notify-send -u low -t 6000 'mbsync' '  failed to fetch mail' >/dev/null 2>&1
-}&
-gsleep 0.2
-kill -74 $(pidof dwmblocks) >/dev/null 2>&1
-fg
+# }&
+# gsleep 0.2
+# kill -74 $(pidof dwmblocks) >/dev/null 2>&1
+# fg
 pre_count="$(ls "${MAIL:-/var/mail/jozan}"/gmail_br/INBOX/new | wc -l 2>/dev/null)"
 mb1="$(ls "${MAIL:-/var/mail/jozan}"/gmail_oon/INBOX/new | wc -l 2>/dev/null)"
 mb2="$(ls "${MAIL:-/var/mail/jozan}"/gmail_tos/INBOX/new | wc -l 2>/dev/null)"
@@ -34,4 +34,4 @@ if [ $post_count -gt $pre_count ]; then
 fi
 echo $post_count >$mc_file
 sleep 1
-kill -74 $(pidof dwmblocks) >/dev/null 2>&1
+# kill -74 $(pidof dwmblocks) >/dev/null 2>&1
