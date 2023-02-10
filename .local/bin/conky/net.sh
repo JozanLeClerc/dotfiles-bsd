f168655bf38da636ea782f0882907b51cba556e5
Joe <rrbo@proton.me>
2023-02-10 18:13:53 +0100

    up

--- .local/bin/conky/net.sh
+++ .local/bin/conky/net.sh
@@ -36,7 +36,6 @@ ${color #928374}DNS IP: ${font2}${color #ebdbb2}${nameserver 0}${font}
 ${color #928374}WAN IP: ${color #ebdbb2}${font2}${execi 300 curl -s https://ifconfig.me || echo No WAN}${font}
 ${color #928374}---------
 ${color #928374}Speed: ${font2}down: ${color #ebdbb2}${font2}${downspeed '$if_main'} ${color #928374}- up: ${color #ebdbb2}${font2}${upspeed '$if_main'}${font}
-${color #928374}Down: ${color #ebdbb2}${downspeedgraph '$if_main' bfbfbf c0c0c0  125829120}
-${color #928374}Up:   ${color #ebdbb2}${upspeedgraph   '$if_main' bfbfbf c0c0c0  125829120}'
+${offset 4}${color #ebdbb2}${downspeedgraph '$if_main' 30,182 bfbfbf c0c0c0 125829120 -t}${goto 198}${color #ebdbb2}${upspeedgraph '$if_main' 30,182 bfbfbf c0c0c0 125829120 -t}'
 
 # ${color #928374}VPN IP:    ${font2}${color #ebdbb2}${addr proton0}${font}
