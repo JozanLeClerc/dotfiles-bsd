7d9805fffb8159bcc1b0c84f89436f63cd5b098e
Joe <bousset.rudy@gmail.com>
2023-02-21 20:01:30 +0100

    up

--- .local/bin/conky/net.sh
+++ .local/bin/conky/net.sh
@@ -27,13 +27,11 @@ ${color #928374}No network${font}'
 fi
 
 echo '${font1}NET ${hr 2}${font}
-${color #928374}Interface: ${font2}${color #ebdbb2}'$if_main'${font}
-${color #928374}---------
+${color #928374}Interface: ${font2}${color #ebdbb2}'$if_main'${font}${goto 175}${color #928374}${font}D: ${color #ebdbb2}${font2}${downspeed '$if_main'}${alignr}${color #928374}${font}Total: ${color #ebdbb2}${font2}${totaldown '$if_main'}${font}
+${color #928374}---------${goto 175}${color #928374}${font}U: ${color #ebdbb2}${font2}${upspeed '$if_main'}${font}${alignr}${color #928374}${font}Total: ${color #ebdbb2}${font2}${totalup '$if_main'}${font}
 ${color #928374}LAN IP: ${font2}${color #ebdbb2}${addr '$if_main'}${font}
 ${color #928374}DNS IP: ${font2}${color #ebdbb2}${nameserver 0}${font}
-${color #928374}WAN IP: ${color #ebdbb2}${font2}${execi 300 curl -s https://ifconfig.me || echo No WAN}${font}
-${color #928374}---------
-${color #928374}${font}Down:  ${color #ebdbb2}${font2}${downspeed '$if_main'}${goto 195}${color #928374}${font}Up:    ${color #ebdbb2}${font2}${upspeed '$if_main'}${font}
-${color #928374}${font}Total: ${color #ebdbb2}${font2}${totaldown '$if_main'}${goto 195}${color #928374}${font}Total: ${color #ebdbb2}${font2}${totalup '$if_main'}${font}'
+${color #928374}WAN IP: ${color #ebdbb2}${font2}${execi 300 curl -s https://ifconfig.me || echo No WAN}${font}'
+
 
 # ${color #928374}VPN IP:    ${font2}${color #ebdbb2}${addr proton0}${font}
