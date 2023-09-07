0819e1ad60ad6c260e670ca676e417e09110ed5c
Joe <rrbo@proton.me>
2023-09-07 13:20:33 +0200

    up

--- .config/nvim/lua/plugs-config/quickscope.lua
+++ .config/nvim/lua/plugs-config/quickscope.lua
@@ -1,7 +1,7 @@
-" Quickscope
-let g:qs_highlight_on_keys = ['f', 'F', 't', 'T']
-
+-- Quickscope
+vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
+vim.g.qs_max_chars=150
+vim.cmd [[
 highlight QuickScopePrimary guifg='#00C7DF' gui=underline ctermfg=155 cterm=underline
 highlight QuickScopeSecondary guifg='#afff5f' gui=underline ctermfg=81 cterm=underline
-
-let g:qs_max_chars=150
+]]
