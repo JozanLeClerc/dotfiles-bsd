d5f27b49bd079f17dfa4e5395c0cb9b1e18143ef
Joe <rrbo@proton.me>
2023-09-08 16:13:42 +0200

    up

--- .config/nvim/lua/plugs-config/rainbow.lua
+++ .config/nvim/lua/plugs-config/rainbow.lua
@@ -1,2 +1,23 @@
 -- Rainbow
-vim.g.rainbow_active = 1
+-- This module contains a number of default definitions
+local rainbow_delimiters = require 'rainbow-delimiters'
+
+vim.g.rainbow_delimiters = {
+    strategy = {
+        [''] = rainbow_delimiters.strategy['global'],
+        vim = rainbow_delimiters.strategy['local'],
+    },
+    query = {
+        [''] = 'rainbow-delimiters',
+        lua = 'rainbow-blocks',
+    },
+    highlight = {
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterRed',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterViolet',
+        'RainbowDelimiterCyan',
+    },
+}
