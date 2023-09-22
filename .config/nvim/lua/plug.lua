fff745c4006d719cb84afe665c49713111bcb4f6
Joe <rrbo@proton.me>
2023-09-22 17:07:35 +0200

    up

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -55,5 +55,6 @@ Plug('hrsh7th/nvim-cmp')
 Plug('hrsh7th/cmp-nvim-lsp')
 Plug('L3MON4D3/LuaSnip')
 Plug('VonHeikemen/lsp-zero.nvim', {['branch'] = 'v3.x'})
+Plug('onsails/lspkind.nvim')
 
 vim.call('plug#end')
