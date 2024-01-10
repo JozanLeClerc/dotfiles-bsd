c9b0b8439541804154fd034e26c5fedb57a59bb9
Joe <rbo@gmx.us>
2024-01-10 16:43:08 +0100

    up

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -5,7 +5,7 @@ tele.setup {
     mappings = {
       i = {
         ['<C-u>'] = false,
-        ['<C-d>'] = false,
+        ['<C-d>'] = require('telescope.actions').delete_buffer,
         ["<C-j>"] = require('telescope.actions').move_selection_next,
         ["<C-k>"] = require('telescope.actions').move_selection_previous,
       },
