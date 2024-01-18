f869377524dbf3d42440b505a571a89d1700d5b6
Joe <rbo@gmx.us>
2024-01-18 13:27:23 +0100

    up

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,7 +15,7 @@ Confdir = os.getenv('XDG_CONFIG_HOME')
 
 vim.cmd('set runtimepath^=/home/r_bousset/.config/nvim runtimepath+=/home/jozan/.config/nvim/after')
 vim.cmd('let &packpath = &runtimepath')
-require('pack')
+require('packs')
 require('settings')
 require('bindings')
 require('langs')
